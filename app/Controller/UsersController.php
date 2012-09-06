1428be9f6349146859aebac50f44f8afa8325d27
Nicolas RAMY-SEPOU <nicolas.ramy@gmail.com>
2012-09-06 23:37:37 +0200

    Update Users controller

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -13,7 +13,8 @@ class UsersController extends AppController {
  * @return void
  */
 	public function index() {
-		return $this->redirect(array('action' => 'login'));
+		$this->User->recursive = 0;
+		$this->set('users', $this->paginate());
 	}
 
 /**
@@ -104,96 +105,6 @@ class UsersController extends AppController {
  * @return void
  */
 	public function delete($id = null) {
-		if (!$this->request->is('post')) {
-			throw new MethodNotAllowedException();
-		}
-		$this->User->id = $id;
-		if (!$this->User->exists()) {
-			throw new NotFoundException(__('Invalid user'));
-		}
-		if ($this->User->delete()) {
-			$this->Session->setFlash(__('User deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('User was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
-
-/**
- * d3admin_index method
- *
- * @return void
- */
-	public function d3admin_index() {
-		$this->User->recursive = 0;
-		$this->set('users', $this->paginate());
-	}
-
-/**
- * d3admin_view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function d3admin_view($id = null) {
-		$this->User->id = $id;
-		if (!$this->User->exists()) {
-			throw new NotFoundException(__('Invalid user'));
-		}
-		$this->set('user', $this->User->read(null, $id));
-	}
-
-/**
- * d3admin_add method
- *
- * @return void
- */
-	public function d3admin_add() {
-		if ($this->request->is('post')) {
-			$this->User->create();
-			if ($this->User->save($this->request->data)) {
-				$this->Session->setFlash(__('The user has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The user could not be saved. Please, try again.'));
-			}
-		}
-	}
-
-/**
- * d3admin_edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function d3admin_edit($id = null) {
-		$this->User->id = $id;
-		if (!$this->User->exists()) {
-			throw new NotFoundException(__('Invalid user'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->User->save($this->request->data)) {
-				$this->Session->setFlash(__('The user has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The user could not be saved. Please, try again.'));
-			}
-		} else {
-			$this->request->data = $this->User->read(null, $id);
-		}
-	}
-
-/**
- * d3admin_delete method
- *
- * @throws MethodNotAllowedException
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function d3admin_delete($id = null) {
 		if (!$this->request->is('post')) {
 			throw new MethodNotAllowedException();
 		}
