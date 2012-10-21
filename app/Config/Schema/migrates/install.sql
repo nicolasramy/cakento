5f881ebc94815e6ad99508bb7730a6e2c1c47a94
Nicolas RAMY-SEPOU <nicolas.ramy@gmail.com>
2012-10-21 22:21:43 +0200

    Update database sql schema

--- app/Config/Schema/migrates/install.sql
+++ app/Config/Schema/migrates/install.sql
@@ -1,1 +1,39 @@
-CREATE DATABASE `cakento` COLLATE 'utf8_general_ci';
+-- Adminer 3.3.4 MySQL dump
+
+SET NAMES utf8;
+SET foreign_key_checks = 0;
+SET time_zone = 'SYSTEM';
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP DATABASE IF EXISTS `cakento`;
+CREATE DATABASE `cakento` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `cakento`;
+
+DROP TABLE IF EXISTS `user_groups`;
+CREATE TABLE `user_groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `archived` tinyint(1) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_group_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `firstname` varchar(128) NOT NULL,
+  `lastname` varchar(128) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `archived` tinyint(1) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- 2012-10-21 22:16:15
