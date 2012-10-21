a858075b70110c81f4d42bf2e2bcc436e8ccb50c
Nicolas RAMY-SEPOU <nicolas.ramy@gmail.com>
2012-10-21 22:23:31 +0200

    Remove obsolete Behavior

--- app/Model/Behavior/CustomerEntityBehavior.php
+++
@@ -1,7 +0,0 @@
-<?php
-class CustomerEntityBehavior extends ModelBehavior {
-    //public $mapMethods = array('/do(\w+)/' => 'doSomething');
-
-    public function setup(Model $Model, $settings = array()) {
-    }
-}
