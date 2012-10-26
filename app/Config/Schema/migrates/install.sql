3e90a335aa24e2a43ba72a1b67ac4adb3918cd1d
Nicolas RAMY-SEPOU <nicolas.ramy-sepou@joliebox.com>
2012-10-26 19:13:47 +0200

    [Schema] Update schema database

--- app/Config/Schema/migrates/install.sql
+++ app/Config/Schema/migrates/install.sql
@@ -1,4 +1,4 @@
--- Adminer 3.5.1 MySQL dump
+-- Adminer 3.6.1 MySQL dump
 
 SET NAMES utf8;
 SET foreign_key_checks = 0;
@@ -8,6 +8,18 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 DROP DATABASE IF EXISTS `cakento`;
 CREATE DATABASE `cakento` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `cakento`;
+
+DROP TABLE IF EXISTS `attributes`;
+CREATE TABLE `attributes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `visible` tinyint(1) NOT NULL,
+  `searchable` tinyint(1) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 
 DROP TABLE IF EXISTS `countries`;
 CREATE TABLE `countries` (
@@ -31,6 +43,74 @@ CREATE TABLE `currencies` (
   `symbol_right` varchar(4) NOT NULL,
   `decimal_point` varchar(4) NOT NULL,
   `thousand_point` varchar(4) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `manufacturers`;
+CREATE TABLE `manufacturers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `pages`;
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `website_id` int(11) NOT NULL,
+  `visible` tinyint(1) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `product_attributes`;
+CREATE TABLE `product_attributes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `attribute_id` int(11) NOT NULL,
+  `int_value` int(11) NOT NULL,
+  `decimal_value` decimal(10,4) NOT NULL,
+  `boolean_value` tinyint(1) NOT NULL,
+  `string_value` varchar(255) NOT NULL,
+  `text_value` text NOT NULL,
+  `datetime_value` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `product_types`;
+CREATE TABLE `product_types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `visible` tinyint(1) NOT NULL,
+  `searchable` tinyint(1) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_type_id` int(11) NOT NULL,
+  `manufacturer_id` int(11) NOT NULL,
+  `saleable` tinyint(1) NOT NULL,
+  `visible` tinyint(1) NOT NULL,
+  `searchable` tinyint(1) NOT NULL,
+  `price` decimal(40,4) NOT NULL,
+  `weight` decimal(40,4) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
@@ -76,6 +156,7 @@ CREATE TABLE `tax_states` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `state_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
@@ -87,6 +168,7 @@ CREATE TABLE `tax_zones` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `zone_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
@@ -120,6 +202,18 @@ CREATE TABLE `users` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `websites`;
+CREATE TABLE `websites` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `unsecure_url` varchar(255) NOT NULL,
+  `secure_url` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
 DROP TABLE IF EXISTS `zones`;
 CREATE TABLE `zones` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -130,4 +224,4 @@ CREATE TABLE `zones` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
--- 2012-10-25 19:10:48
+-- 2012-10-26 19:12:52
